2eb4c6c021d2ae4ad6b55976fe1171f6b5fa00ea
Eric Funes-Figueroa <eric_funes_figueroa@hotmail.com>
2020-03-31 02:31:13 -0400

    Actions for the drawer manu added

--- app/src/main/java/com/example/friendzone/MainActivity.java
+++ app/src/main/java/com/example/friendzone/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.friendzone;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -7,11 +9,18 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private Toolbar toolbar;
     private DrawerLayout drawer;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,20 +30,58 @@ public class MainActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 
+        drawer = findViewById(R.id.drawer_layout);
         drawer.addDrawerListener(toggle);
+
         toggle.syncState();
     }
 
+    /**
+     * ANDROID BACK BUTTON ACTION
+     * If the drawer is open when we click on the back button, it closes.
+     * Otherwise, it goes to the previous activity.
+     */
     @Override
     public void onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
+        }
+    }
+
+    /**
+     * ACTIONS OF ITEMS IN THE MENU
+     * @param menuItem
+     * @return
+     */
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_settings:
+                getFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsScreen())
+                        .addToBackStack(null)
+                        .commit();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    /**
+     * Loads the settings view.
+     */
+    public static class SettingsScreen extends PreferenceFragment {
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_screen);
         }
     }
 }
