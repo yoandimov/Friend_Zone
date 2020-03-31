42176f4bddb1e8a7838f7d7f61d1300b38aee58e
Eric Funes-Figueroa <eric_funes_figueroa@hotmail.com>
2020-03-30 20:45:24 -0400

    Bug line 18 MainActivity.class

--- app/src/main/java/com/example/friendzone/MainActivity.java
+++ app/src/main/java/com/example/friendzone/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.friendzone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
     }
 }
