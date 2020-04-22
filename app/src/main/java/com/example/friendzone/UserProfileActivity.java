891ae8f5d87b4d47a18dc2b1a54ec0654ae8acc8
Eric Funes-Figueroa <eric_funes_figueroa@hotmail.com>
2020-04-22 06:42:04 -0400

    Gallery intent test added.

--- app/src/main/java/com/example/friendzone/UserProfileActivity.java
+++ app/src/main/java/com/example/friendzone/UserProfileActivity.java
@@ -2,13 +2,20 @@ package com.example.friendzone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.friendzone.Models.User;
 import com.example.friendzone.controller.ControllerUser;
+
+import java.io.IOException;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -20,17 +27,47 @@ public class UserProfileActivity extends AppCompatActivity {
     ImageView profilepic;
     TextView username, post;
 
+    private static final int PICK_IMAGE = 1;
+    Uri imageUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
-        profilepic = (ImageView) findViewById(R.id.imageView);
+        profilepic = (ImageView) findViewById(R.id.profile_picture);
         username = (TextView) findViewById(R.id.username);
         post = (TextView) findViewById(R.id.post);
 
         controllerUser = new ControllerUser(Login.getAuthorization(this));
         controllerUser.getUserInfo(userCallback);
+
+        profilepic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gallery = new Intent();
+                gallery.setType("image/*");
+                gallery.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(Intent.createChooser(gallery, "Select Picture"), PICK_IMAGE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data != null) {
+            imageUri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                profilepic.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private Callback<User> userCallback = new Callback<User>() {
@@ -39,6 +76,7 @@ public class UserProfileActivity extends AppCompatActivity {
             if(response.isSuccessful()) {
                 User user = response.body();
                 String str = String.format("%d %s %s %s", user.userId, user.getFirstName(), user.getUsername(), user.getEmail());
+                username.setText(user.getUsername());
                 Log.d("Userlog", str);
             }
         }
